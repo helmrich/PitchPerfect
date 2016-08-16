36ba31765cf221d5ab1c760cc34909bb6f77e0e9
Tobias Helmrich <me@tobias-helmrich.de>
2016-08-16 23:29:59 +0200

    Update deprecated use of init() with NSURL type

--- Pitch Perfect/RecordedAudio.swift
+++ Pitch Perfect/RecordedAudio.swift
@@ -10,10 +10,10 @@ import Foundation
 
 class RecordedAudio: NSObject {
     
-    var filePathUrl = NSURL!()
+    var filePathUrl: NSURL
     var title: String!
     
-    init(filePathUrl: NSURL!, title: String!) {
+    init(filePathUrl: NSURL, title: String!) {
         self.filePathUrl = filePathUrl
         self.title = title
     }
